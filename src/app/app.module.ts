41e5771ff713642bdc4467253baca713768bc902
Taiga Nakagawa <thomasnakagawa33@gmail.com>
2024-04-27 01:55:54 -0300

    pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {CoreModule} from "./core/core.module";
 import {NgxSpinnerModule} from "ngx-spinner";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpClientModule} from "@angular/common/http";
+import {ToastrModule} from "ngx-toastr";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {HttpClientModule} from "@angular/common/http";
     HttpClientModule,
     AppRoutingModule,
     CoreModule,
-    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,9 +3,15 @@ import { LoginComponent } from "./component/login.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import {LoginRountingModule} from "./login.routing.module";
 import {NgIf} from "@angular/common";
+import {NgxSpinnerModule} from "ngx-spinner";
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [LoginRountingModule, ReactiveFormsModule, NgIf]
+  imports: [
+    LoginRountingModule,
+    ReactiveFormsModule,
+    NgIf,
+    NgxSpinnerModule
+  ]
 })
 export class LoginModule {}
