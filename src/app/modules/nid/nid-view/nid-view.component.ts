6803cfb8b6f68b96c445b165435d1af0c74fdcdd
Taiga Nakagawa <thomasnakagawa33@gmail.com>
2024-04-27 01:57:32 -0300

    pronto

---
+++ src/app/modules/nid/nid-view/nid-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nid-view',
+  templateUrl: './nid-view.component.html',
+  styleUrl: './nid-view.component.scss'
+})
+export class NidViewComponent {
+
+}
