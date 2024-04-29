4b63bb70f9bc7ea5f38d53539e0fb65b6c404556
Taiga Nakagawa <thomasnakagawa33@gmail.com>
2024-04-29 17:09:40 -0300

    tela de relatorio de despesa e refatoracao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AuthGuard} from "./core/guards/auth.guard";
+import {NidViewComponent} from "./modules/nid/nid-view/nid-view.component";
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'nid',
+    loadChildren: () => import('./modules/nid/nid.module').then(m => m.NidViewModule)
   },
   {
     path: '**',
---
+++ src/app/modules/nid/nid.routing.module.ts
@@ -0,0 +1,14 @@
+import {Route, RouterModule} from "@angular/router";
+import {NidViewComponent} from "./nid-view/nid-view.component";
+import {NgModule} from "@angular/core";
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: NidViewComponent
+  }
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)]
+})
+export class NidRoutingModule {}
